e9748675f4d5653a5e513fae6b8812d42f4a7aad
coffeegoddd <dbeaubrown@gmail.com>
2019-01-11 08:19:43 -0800

    Updated git ignore to exclude config file, working on query optimization

--- db/dbHelpers.js
+++ db/dbHelpers.js
@@ -1,27 +1,6 @@
 const db = require('./index.js');
 
 
-const makeNewSummaryQuery = (id) => {
-  return (`SELECT restaurant_id, COUNT(*) total_reviews,
-  CAST(AVG(overall_score) AS DECIMAL(2,1)) avg_overall,
-  CAST(AVG(food_score) AS DECIMAL(2,1)) avg_food,
-  CAST(AVG(service_score) AS DECIMAL(2,1)) avg_service,
-  CAST(AVG(ambience_score) AS DECIMAL(2,1)) avg_ambience,
-  CAST(AVG(value_score) AS DECIMAL(2,1)) avg_value,
-  SUM(would_recommend) / COUNT(*) pct_recommend,
-  "Burgers" review_filter_1,
-  "Burritos" review_filter_2,
-  "Pizzas" review_filter_3,
-  "Tacos" review_filter_4,
-  "Sandwiches" review_filter_5,
-  'Sauces' loved_for_1,
-  'Wine' loved_for_2,
-  'Moderate' noise_level
-  FROM reviews_detail
-  WHERE restaurant_id = ${id}
-  GROUP BY 1`);
-};
-
 module.exports = {
   getAllReviews: restaurantId => db.query(`
     SELECT
@@ -45,7 +24,24 @@ module.exports = {
   // getReviewsSummary: restaurantId => db.query(`
   //   SELECT* FROM reviews_summary WHERE restaurant_id = ${restaurantId};
   // `),
-  getReviewsSummary: restaurantId => db.query(makeNewSummaryQuery(restaurantId)),
+  getReviewsSummary: restaurantId => db.query(`SELECT restaurant_id, COUNT(*) total_reviews,
+    CAST(AVG(overall_score) AS DECIMAL(2,1)) avg_overall,
+    CAST(AVG(food_score) AS DECIMAL(2,1)) avg_food,
+    CAST(AVG(service_score) AS DECIMAL(2,1)) avg_service,
+    CAST(AVG(ambience_score) AS DECIMAL(2,1)) avg_ambience,
+    CAST(AVG(value_score) AS DECIMAL(2,1)) avg_value,
+    SUM(would_recommend) / COUNT(*) pct_recommend,
+    "Burgers" review_filter_1,
+    "Burritos" review_filter_2,
+    "Pizzas" review_filter_3,
+    "Tacos" review_filter_4,
+    "Sandwiches" review_filter_5,
+    'Sauces' loved_for_1,
+    'Wine' loved_for_2,
+    'Moderate' noise_level
+    FROM reviews_detail
+    WHERE restaurant_id = ${restaurantId}
+    GROUP BY 1`),
 };
 
 
